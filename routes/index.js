92b31f59a061d91c34e5a8c989e6d1af4e26b2b5
Gj <gongjie0422@163.com>
2024-04-07 14:55:52 +0800

    init

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,10 @@
 const router = require('koa-router')()
-const { execSync } = require('child_process');
+const { exec } = require('child_process');
 
 router.get('/deploy', async (ctx, next) => {
   const scriptPath = `${__dirname}/deploy.sh`;
-  try {
-    const res = execSync(`sh ${scriptPath}`);
-    ctx.body = { code: 100, message: res.toString() }
-  } catch (error) {
-    const res = execSync(`sh ${scriptPath}`);
-    ctx.body = { code: 100, message: error.toString() }
-  }
+  const res = exec(`sh ${scriptPath}`);
+  ctx.body = { code: 100 }
   next()
 })
 
