078ae1709b9596865f24a1f7c2676f75c3360b56
Gj <gongjie0422@163.com>
2024-04-11 13:44:49 +0800

    调整构建流程

--- router.js
+++ router.js
@@ -1,10 +1,61 @@
 const router = require('koa-router')()
 const { exec } = require('child_process');
 
+let lock = false
+let msgs = []
+
+async function executeShellCommand(command) {
+  return new Promise((resolve, reject) => {
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        reject(error);
+        return;
+      }
+      if (stderr) {
+        resolve(stderr);
+      } else {
+        resolve(stdout);
+      }
+    });
+  });
+}
+
 router.get('/deploy', async (ctx, next) => {
-  const scriptPath = `${__dirname}/deploy.sh`;
-  const res = exec(`sh ${scriptPath}`);
   ctx.body = { code: 100 }
+  if (lock) {
+    next()
+    return
+  }
+  try {
+    lock = true
+    msgs = []
+    await executeShellCommand('cd /home/server/antv-x6-serve')
+    msgs.push("开始构建serve")
+    await executeShellCommand('git pull')
+    msgs.push("拉取服务端最新代码")
+    await executeShellCommand('pnpm i')
+    msgs.push("更新服务端依赖")
+    await executeShellCommand('pm2 reload 0')
+    msgs.push("重启服务")
+    await executeShellCommand('cd /home/webroots/antv-x6-vue3')
+    msgs.push("开始构建前端")
+    await executeShellCommand('git pull')
+    msgs.push("拉取编辑器最新代码")
+    await executeShellCommand('pnpm i')
+    msgs.push("更新编辑器依赖")
+    await executeShellCommand('pnpm build')
+    msgs.push("开始编译")
+    await executeShellCommand('nginx -s reload')
+    msgs.push("构建完成")
+    lock = false
+  } catch (error) {
+    console.log("[debug]error:", error)
+  }
+  next()
+})
+
+router.get('/deploy-status', async (ctx, next) => {
+  ctx.body = { code: 100, data: msgs }
   next()
 })
 
